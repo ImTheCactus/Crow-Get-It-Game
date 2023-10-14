593b0c891c57b35e3fb4e5f8ff837e6fc3492f81
OceanNogg <127952964+OceanNogg@users.noreply.github.com>
2023-10-14 22:22:48 +0800

    Quest Journal reintegration

---
+++ Unity/Crow Get It/Assets/Scripts/CustomYarnCommands.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class CustomYarnCommands : MonoBehaviour
+{
+public bool mushQuestActive = false;
+public bool chipQuestActive = false;
+public bool queenieQuestActive = false;
+public bool bubblesQuestActive = false;
+public bool raphQuestActive = false;
+public bool willowQuestActive = false;
+public bool scarecrowStrawQuestActive = false;
+public bool scarecrowButtonQuestActive = false;
+public bool scarecrowHatQuestActive = false;
+public DialogueRunner dialogueRunner;
+
+ void Start()
+ {
+    //Custom Yarn commands           (Name for in yarn, function to run)
+    // Name for in Yarn [CAN] be differant to the function name
+    dialogueRunner.AddCommandHandler("MushQuestSwitch", MushQuestSwitch);
+    dialogueRunner.AddCommandHandler("ChipQuestSwitch", ChipQuestSwitch);
+    dialogueRunner.AddCommandHandler("QueenieQuestSwitch", QueenieQuestSwitch);
+    dialogueRunner.AddCommandHandler("BubblesQuestSwitch", BubblesQuestSwitch);
+    dialogueRunner.AddCommandHandler("RaphQuestSwitch", RaphQuestSwitch);
+    dialogueRunner.AddCommandHandler("WillowQuestSwitch", WillowQuestSwitch);
+    dialogueRunner.AddCommandHandler("ScarecrowStrawSwitch", ScarecrowStrawSwitch);
+    dialogueRunner.AddCommandHandler("ScarecrowButtonSwitch", ScarecrowButtonSwitch);
+    dialogueRunner.AddCommandHandler("ScarecrowHatSwitch", ScarecrowHatSwitch);
+    
+ }
+
+    public void MushQuestSwitch()
+    {
+        if (mushQuestActive == false)
+        {
+            mushQuestActive = true;
+            Debug.Log("quest switch mush");
+        }
+        else
+        {
+            mushQuestActive = false;
+        }
+
+    }
+
+
+    public void ChipQuestSwitch()
+    {
+        if (chipQuestActive == false)
+        {
+           chipQuestActive = true;
+            Debug.Log("quest switch chip");
+        }
+        else
+        {
+            chipQuestActive = false;
+        }
+    }
+
+     public void QueenieQuestSwitch()
+    {
+        if (queenieQuestActive == false)
+        {
+           queenieQuestActive = true;
+            Debug.Log("quest switch queenie");
+        }
+        else
+        {
+            queenieQuestActive = false;
+        }
+    }
+
+
+     public void BubblesQuestSwitch()
+    {
+        if (bubblesQuestActive == false)
+        {
+           bubblesQuestActive  = true;
+            Debug.Log("quest switch queenie");
+        }
+        else
+        {
+            bubblesQuestActive  = false;
+        }
+    }
+
+
+      public void RaphQuestSwitch()
+    {
+        if (raphQuestActive == false)
+        {
+           raphQuestActive  = true;
+            Debug.Log("quest switch raph");
+        }
+        else
+        {
+            raphQuestActive  = false;
+        }
+    }
+
+
+        public void WillowQuestSwitch()
+    {
+        if (willowQuestActive == false)
+        {
+           willowQuestActive  = true;
+            Debug.Log("quest switch willow");
+        }
+        else
+        {
+            willowQuestActive  = false;
+        }
+    }
+    
+     public void ScarecrowStrawSwitch()
+    {
+        if (scarecrowStrawQuestActive == false)
+        {
+           scarecrowStrawQuestActive  = true;
+            Debug.Log("quest switch scarecrow");
+        }
+        else
+        {
+            scarecrowStrawQuestActive  = false;
+        }
+    }
+
+
+ public void ScarecrowButtonSwitch()
+    {
+        if (scarecrowButtonQuestActive == false)
+        {
+           scarecrowButtonQuestActive  = true;
+            Debug.Log("quest switch scarecrow");
+        }
+        else
+        {
+            scarecrowButtonQuestActive  = false;
+        }
+    }
+
+
+     public void ScarecrowHatSwitch()
+    {
+        if (scarecrowHatQuestActive == false)
+        {
+           scarecrowHatQuestActive  = true;
+            Debug.Log("quest switch scarecrow");
+        }
+        else
+        {
+            scarecrowHatQuestActive  = false;
+        }
+    }
+
+
+}
--- Unity/Crow Get It/Assets/Scripts/Quest_Journal.cs
+++ Unity/Crow Get It/Assets/Scripts/Quest_Journal.cs
@@ -7,32 +7,167 @@ public class Quest_Journal : MonoBehaviour
 {
     //public string key;
     public GameObject tabMenu;
+    //public GameObject menuButton;
+    public GameObject MushQuest;
+    public GameObject ChipQuest;
+    public GameObject QueenieQuest;
+    public GameObject BubblesQuest;
+    public GameObject RaphQuest;
+    public GameObject WillowQuest;
+    public GameObject ScarecrowStrawQuest;
+    public GameObject ScarecrowButtonQuest;
+    public GameObject ScarecrowHatQuest;
+    
+
+    public CustomYarnCommands customYarnCommands;
+
+    bool mushQuestActive = false;
+    bool chipQuestActive = false;
+    bool queenieQuestActive = false;
+    bool bubblesQuestActive = false;
+    bool raphQuestActive = false;
+    bool willowQuestActive = false;
+    bool scarecrowStrawQuestActive = false;
+    bool scarecrowButtonQuestActive = false;
+    bool scarecrowHatQuestActive = false;
 
     private bool isOpen;
 
     void Start()
     {
         isOpen = false;
+        Cursor.visible = false;
+        MushQuest.SetActive(false);
+        ChipQuest.SetActive(false);
+        QueenieQuest.SetActive(false);
+        BubblesQuest.SetActive(false);
+        RaphQuest.SetActive(false);
+        WillowQuest.SetActive(false); 
+        ScarecrowStrawQuest.SetActive(false);
+        ScarecrowButtonQuest.SetActive(false);
+        ScarecrowHatQuest.SetActive(false);
+       
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown("tab"))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
             if (isOpen == false)
             {
                 tabMenu.SetActive(true);
+
+                //menuButton.SetActive(false);
                 isOpen = true;
+                //Cursor.lockState = CursorLockMode.None;
+                //Cursor.visible = true;
 
             }
             else
             {
                 tabMenu.SetActive(false);
+                //menuButton.SetActive(true);
                 isOpen = false;
+                //Cursor.visible = false;
             }
             //EventSystem.current.SetSelectedGameObject(this.gameObject);
         }
 
+        QuestsCheck();
+
     } 
     
+    public void QuestsCheck()
+    {
+        
+        if (customYarnCommands.mushQuestActive == true)
+        {
+            MushQuest.SetActive(true);
+        }
+        else if (customYarnCommands.mushQuestActive == false)
+        {
+            MushQuest.SetActive(false);
+        }
+
+        if (customYarnCommands.chipQuestActive == true)
+        {
+            ChipQuest.SetActive(true);
+        }
+        else if (customYarnCommands.chipQuestActive == false)
+        {
+            ChipQuest.SetActive(false);
+        }
+
+
+         if (customYarnCommands.queenieQuestActive == true)
+        {
+            QueenieQuest.SetActive(true);
+        }
+        else if (customYarnCommands.queenieQuestActive == false)
+        {
+            QueenieQuest.SetActive(false);
+        }
+
+
+         if (customYarnCommands.bubblesQuestActive == true)
+        {
+            BubblesQuest.SetActive(true);
+        }
+        else if (customYarnCommands.bubblesQuestActive == false)
+        {
+            BubblesQuest.SetActive(false);
+        }
+
+          if (customYarnCommands.raphQuestActive == true)
+        {
+             RaphQuest.SetActive(true);
+        }
+        else if (customYarnCommands.raphQuestActive == false)
+        {
+             RaphQuest.SetActive(false);
+        }
+
+
+         if (customYarnCommands.willowQuestActive == true)
+        {
+             WillowQuest.SetActive(true);
+        }
+        else if (customYarnCommands.willowQuestActive == false)
+        {
+             WillowQuest.SetActive(false);
+        }
+
+
+         if (customYarnCommands.scarecrowStrawQuestActive == true)
+        {
+             ScarecrowStrawQuest.SetActive(true);
+        }
+        else if (customYarnCommands.scarecrowStrawQuestActive == false)
+        {
+             ScarecrowStrawQuest.SetActive(false);
+        }
+
+
+          if (customYarnCommands.scarecrowButtonQuestActive == true)
+        {
+             ScarecrowButtonQuest.SetActive(true);
+        }
+        else if (customYarnCommands.scarecrowButtonQuestActive == false)
+        {
+             ScarecrowButtonQuest.SetActive(false);
+        }
+
+
+            if (customYarnCommands.scarecrowHatQuestActive == true)
+        {
+             ScarecrowHatQuest.SetActive(true);
+        }
+        else if (customYarnCommands.scarecrowHatQuestActive == false)
+        {
+             ScarecrowHatQuest.SetActive(false);
+        }
+
+
+    }
+
 }
